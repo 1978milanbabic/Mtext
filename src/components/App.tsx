a2153e5984a249270e9361278810264198e4d9fc
1978milanbabic <1978milan.babic@gmail.com>
2023-12-19 04:37:24 +0100

    [Mtext] basic functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,8 +10,8 @@ const styles_1 = {
   height: '300px',
   color: 'gray',
   fontSize: '20px',
-  overflow: 'hidden',
-  lineHeight: '1.5',
+  lineHeight: '0.9',
+  fontFamily: 'arial',
 } as CSSProperties;
 
 const styles_2 = {
@@ -20,27 +20,48 @@ const styles_2 = {
   left: '550px',
   width: '350px',
   height: '300px',
-  color: '#666',
+  color: '#999',
   fontSize: '25px',
-  overflow: 'hidden',
+  textAlign: 'justify',
 } as CSSProperties;
 
 const App: React.FC = () => {
+  // obligatory!
+  const [nmbToSolve, setNmbToSolve] = useState<number>(0);
+
+  // ** testing purpose **
+  useEffect(() => {
+    console.log('🚀 -> number to solve: ', nmbToSolve);
+  }, [nmbToSolve]);
+
   return (
     <div>
-      <Mtext as={'catch'} style={styles_1}>
+      <Mtext
+        as={'catch'}
+        style={styles_1}
+        setNmbToSolve={setNmbToSolve}
+        abbrText={'Warning text!!! Warning text!!! Warning text!!!'}
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud <span>exercitation</span> ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        tempor incididunt ut labore et dolore magna aliqua. <br />
+        Ut enim ad minim veniam, quis nostrud <span>exercitation</span> ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. <br />
+        <br />
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
       </Mtext>
-      <Mtext as={'catch'} style={styles_2}>
+      <Mtext
+        as={'catch'}
+        style={styles_2}
+        setNmbToSolve={setNmbToSolve}
+        abbrText={'Warning text!!! Warning text!!! Warning text!!!'}
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor <span>incididunt</span> ut labore et dolore magna aliqua. Ut enim
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        ad minim veniam, quis nostrud exercitation <span>ullamco</span> laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
       </Mtext>
     </div>
   );
--- src/components/Mtext/index.tsx
+++ src/components/Mtext/index.tsx
@@ -5,54 +5,146 @@ import React, {
   useState,
   useEffect,
   SetStateAction,
+  Dispatch,
+  useMemo,
+  MouseEvent,
 } from 'react';
-import parse from 'html-react-parser';
-// component specifyc styles
-import './Mtext.module.scss';
+import parse, { domToReact } from 'html-react-parser';
+import uuid from 'react-uuid';
+// component specific styles
+import './Mtext.scss';
 
 type Props = {
-  children?: ReactNode | null;
+  children?: ReactNode | string | null;
   as: string;
   style?: CSSProperties | null;
+  setNmbToSolve: Dispatch<SetStateAction<number>>;
+  abbrText?: string;
 };
 
-const Mtext = ({ children, as, style }: Props) => {
+const Mtext = ({ children, as, style, setNmbToSolve, abbrText }: Props) => {
+  const uniqueId = uuid();
+  const [initionalContent, setInitionalcontent] = useState<ReactNode | null>(
+    null,
+  );
   const [innerContent, setInnerContent] = useState<ReactNode | null>(null);
   const [innerText, setInnerText] = useState<string>('');
-  const wrapperRef = useRef();
+  const [solvePerComponent, setSolvePerComponent] = useState<number>(0);
+  const wrapperRef = useRef<HTMLDivElement>();
+
+  // click on mandatory edit
+  const handleClick = (e: MouseEvent) => {
+    const span = e.target as HTMLSpanElement;
+    // set parent state to 'resolved' -> if not already resolved
+    if (span.className === 'unresolved') {
+      setNmbToSolve((nmbToSolve) => nmbToSolve - 1);
+      setSolvePerComponent((solvePerComponent) => solvePerComponent - 1);
+    }
+    // set className to 'resolved'
+    span.className = '';
+    // set spans ready for editing
+    span.contentEditable = 'true';
+    // set auto-selected text
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.setStart(span, 0);
+    range.setEnd(span, 1);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    // discard parent abbr
+    const abbr: HTMLElement | null = span?.parentElement;
+    if (abbr?.title) abbr.title = '';
+  };
+
+  // initional content
+  useMemo(() => {
+    setInitionalcontent(children);
+  }, []);
 
   useEffect(() => {
-    setInnerContent(children);
-  }, [children]);
-
-  // useEffect(() => {
-  //   innerContent && console.log(innerContent);
-  // }, [innerContent]);
+    // parse to html tags
+    const content = document.querySelector(
+      `[data-unique = "${uniqueId}"]`,
+    )?.innerHTML;
+    // get initional content and modify to starting model
+    const options = {
+      replace(domNode: any) {
+        const { attribs, children: childs } = domNode;
+        if (domNode?.name === 'span') {
+          // increase nmb for solve
+          setNmbToSolve((nmbToSolve) => nmbToSolve + 1);
+          // set memory per component
+          setSolvePerComponent((solvePerComponent) => solvePerComponent + 1);
+          // set spans for editing
+          return (
+            <abbr title={abbrText}>
+              <span
+                className='unresolved'
+                contentEditable={false}
+                onClick={(e) => handleClick(e)}
+              >
+                {domToReact(childs, options)}
+              </span>
+            </abbr>
+          );
+        }
+      },
+    };
+    // set react DOM
+    setInnerContent(parse(content, options));
+  }, [initionalContent]);
 
   const handleTyping = (e: any) => {
+    // check for unresolved number in case of text deletion
+    const virtualModifiedElement: HTMLDivElement =
+      document.createElement('div');
+    virtualModifiedElement.innerHTML = e;
+    let countUnresolved = 0;
+    const options = {
+      replace(domNode: any) {
+        const { attribs, children: childs } = domNode;
+        if (domNode?.name === 'span' && attribs.class === 'unresolved') {
+          countUnresolved++;
+          return (
+            <span
+              className='unresolved'
+              contentEditable={false}
+              onClick={(e) => handleClick(e)}
+            >
+              {domToReact(childs, options)}
+            </span>
+          );
+        }
+      },
+    };
+    let parsedEl = parse(e, options);
+    let localyResolvedChanged = solvePerComponent - countUnresolved;
+    // set nmbTo solve
+    setSolvePerComponent(
+      (solvePerComponent) => solvePerComponent - localyResolvedChanged,
+    );
+    setNmbToSolve((nmbToSolve) => nmbToSolve - localyResolvedChanged);
+    // set text
     setInnerText(e);
-    console.log(e);
   };
 
   return (
     <>
       <div
+        className='text-for-editing'
         style={style}
         contentEditable={true}
-        onInput={(e) => handleTyping(e.currentTarget.textContent)}
+        onInput={(e) => handleTyping(e.currentTarget.innerHTML)}
         ref={wrapperRef}
       >
         {innerContent}
       </div>
-      {/* TEST DIV -> REMOVE THIS AND <></> */}
       <div
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-        }}
+        className='initional-content'
+        style={{ display: 'none' }}
+        data-unique={uniqueId}
       >
-        {parse(innerText)}
+        {initionalContent}
       </div>
     </>
   );
